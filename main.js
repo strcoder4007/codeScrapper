9cd6f5b9872a162da642aeb0d6040abfebab09b2
Shubham Singh <mynameshubham@gmail.com>
2017-10-05 22:58:20 +0400

    frontend almost done

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let mainWindow;
 //Listen for the app to be ready
 app.on('ready', function(){
     //create new window
-    mainWindow = new BrowserWindow({backgroundColor: '#222', frame: false, titleBarStyle: 'hidden', maxWidth: 800, maxHeight: 450, width: 800, height: 450, minWidth: 600, minHeight: 300});
+    mainWindow = new BrowserWindow({backgroundColor: '#222', frame: false, titleBarStyle: 'hidden', maxWidth: 800, maxHeight: 450, width: 800, height: 450, minWidth: 800, minHeight: 450});
     //load html into window
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'mainWindow.html'),
